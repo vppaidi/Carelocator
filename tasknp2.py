44ee94cc871144053d8796f38f5837bb3a34d718
Vijay Paidi <34457575+vppaidi@users.noreply.github.com>
2025-10-04 00:18:26 +0200

    Update tasknp2.py

--- tasknp2.py
+++ tasknp2.py
@@ -34,13 +34,9 @@ from redis import Redis
 from rq import get_current_job
 
 
-REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
-REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
-REDIS_DB = int(os.getenv('REDIS_DB', 0))
-REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', None)
-REDIS_SSL = os.getenv('REDIS_SSL', 'False') == 'True'
-
-redis_conn = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD, ssl=REDIS_SSL)
+REDIS_URL = os.getenv("REDIS_URL", "redis://127.0.0.1:6379/0")
+# Redis.from_url understands rediss:// and TLS on port 6380
+redis_conn = Redis.from_url(REDIS_URL)
 
 def recommend_task2(selected_dropdown, P_FACILITIES, dm, wei, addresses, mode="pmedian"):
     job = get_current_job()
@@ -168,3 +164,4 @@ def recommend_task2(selected_dropdown, P_FACILITIES, dm, wei, addresses, mode="p
         redis_conn.set(f"error_for_job_{job_id}", error_message)
 
         return "Task failed"
+
